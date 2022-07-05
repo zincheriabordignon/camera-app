99d6d71768910a4383bfe584c9d95567766ec178
zincheriabordignon <77843598+zincheriabordignon@users.noreply.github.com>
2022-07-05 17:55:23 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -10,10 +10,62 @@ const canvas = document.getElementById('canvas');
 const photos = document.getElementById('photos');
 const photoButton = document.getElementById('photo-button');
 const clearButton = document.getElementById('clear-button');
+const cameraOptions = document.querySelector('.video-options>select');
 const photoFilter = document.getElementById('photo-filter');
 
+const getCameraSelection = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  const options = videoDevices.map(videoDevice => {
+    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+  });
+  cameraOptions.innerHTML = options.join('');
+};
+
+const constraints = {
+  video: {
+    width: {
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440
+    },
+  }
+};
+
+
 // Get media stream
-navigator.mediaDevices.getUserMedia({video: true, audio: false})
+const startStream = async (constraints) => {
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+  handleStream(stream);
+};
+
+const handleStream = (stream) => {
+  video.srcObject = stream;
+  video.play();
+  //play.classList.add('d-none');
+  //pause.classList.remove('d-none');
+  //screenshot.classList.remove('d-none');
+  //streamStarted = true;
+};
+
+cameraOptions.onchange = () => {
+  const updatedConstraints = {
+    ...constraints,
+    deviceId: {
+      exact: cameraOptions.value
+    }
+  };
+  startStream(updatedConstraints);
+};
+
+startStream(constraints);
+
+/* navigator.mediaDevices.getUserMedia({video: { facingMode: 'environment' }, audio: false})
   .then(function(stream) {
     // Link to the video source
     video.srcObject = stream;
@@ -23,7 +75,7 @@ navigator.mediaDevices.getUserMedia({video: true, audio: false})
   .catch(function(err) {
     console.log(`Error: ${err}`);
   });
-
+ */
   // Play when ready
   video.addEventListener('canplay', function(e) {
     if(!streaming) {
@@ -95,3 +147,5 @@ navigator.mediaDevices.getUserMedia({video: true, audio: false})
       photos.appendChild(img);
     }
   }
+
+  getCameraSelection();
